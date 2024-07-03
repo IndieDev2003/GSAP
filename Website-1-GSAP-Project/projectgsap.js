08e51cdd2beaee8f5394b5b7bd94324a0ac7408d
Gagan Suman <ilovescience584@gmail.com>
2024-07-03 19:39:00 +0530

    tried to add scroll trigger

--- Website-1-GSAP-Project/projectgsap.js
+++ Website-1-GSAP-Project/projectgsap.js
@@ -1,15 +1,26 @@
 let navTimeline = gsap.timeline();
 
-navTimeline.from('.nav a,.location',{
-    y:-100,
-    opacity:0,
-    duration:0.5,
-    stagger:0.15
-})
-navTimeline.from('.hero .play',{
-    y:1000,
-    // rotate:360,
-    duration:1,
-    opacity:0,
-    stagger:0.15
-})
+navTimeline.from(".nav a,.location", {
+  y: -100,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.15,
+});
+navTimeline.from(".hero .play", {
+  y: 1000,
+  // rotate:360,
+  duration: 1,
+  opacity: 0,
+  stagger: 0.15,
+});
+gsap.from(".cards", {
+  x: -1000,
+  opacity: 0,
+  stagger: 0.2,
+  scrollTrigger: {
+    scroller: ".cards",
+    trigger: "body",
+    markers: true,
+    start: "top 50%",
+  },
+});
